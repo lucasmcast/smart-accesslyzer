b51f5039637983bc5fa072f2db3ed8efdad1df3e
Lucas Martins <lucas.martins.c03@gmail.com>
2023-10-03 21:02:42 -0300

    Removido arquivos desnecessarios

--- XDG_CACHE_HOME/Microsoft/Python Language Server/stubs.v1/gLQI8lLlHNwalfjgtWKqInudvHLggmNdMuCRofQYWAg=/_imagingtk.cpython-36m-x86_64-linux-gnu.pyi
+++
@@ -1,7 +0,0 @@
-__doc__ = None
-__file__ = '/home/lucas/anaconda3/envs/curso_dlib/lib/python3.6/site-packages/PIL/_imagingtk.cpython-36m-x86_64-linux-gnu.so'
-__name__ = 'PIL._imagingtk'
-__package__ = 'PIL'
-def tkinit():
-    pass
-
